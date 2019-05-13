b8d0106fc9308e9ae5f16ceb7727a9ce98edcd2f
ZZZ <monkey.d.chong@gmail.com>
2019-05-14 00:46:35 +0700

    2 user

--- botpush.php
+++ botpush.php
@@ -9,12 +9,15 @@ $access_token = 'Cg5ODfaoolXn/2qKtY81USq6NRCtbt/CmjNAAddhYQdeBRw/DMXVrmu8u09fL1Z
 $channelSecret = 'f436a2bbf4723459c3400b1fc30b2a9a';
 
 $pushID = 'U13d72a202b537a08c4db39fdce4db90f';
+$pushID2 = 'U2d9e8d7fb8876f55fb76c5f1f04bb962';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
+
+$response = $bot->pushMessage($pushID2, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
